2d3eb973ee6ba828fce0fd7c8a794153dc149e92
csusanacp <claudia.cisneros@enroutesystems.com>
2022-01-13 20:12:05 -0600

    Second commit-Class Definition-get/set

--- main.py
+++ main.py
@@ -1,4 +1,77 @@
 # object oriented programming exercise
 
 class Shape:
-    def __init__(self, color, filled):
+    def __init__(self, __color, __filled):
+        self.__color = __color
+        self.__filled = __filled
+
+    def get_color(self):
+        return self.__color
+
+    def set_color(self,color):
+        self.__color = color
+
+    def get_filled(self):
+        return self.__filled
+
+    def set_filled(self,filled):
+        self.__filled = filled
+
+
+class Circle(Shape):
+    def __init__(self, __color, __filled, __radius):
+        Shape.__init__(self, __color, __filled)
+        self.__radius= __radius
+
+    def get_radius(self):
+        return self.__radius
+
+    def set_radius(self, radius):
+        self.__radius = radius
+
+
+class Rectangle(Shape):
+
+    def __init__(self, __color, __filled, __width, __length):
+        Shape.__init__(self, __color, __filled)
+        self.__width = __width
+        self.__length = __length
+
+    def get_width(self):
+        return self.__width
+
+    def set_width(self, width):
+        self.__width = width
+
+    def get_length(self):
+        return self.__length
+
+    def set_length(self,length):
+        self.__length = length
+
+
+class Square (Rectangle):
+
+    def __init__(self, __color, __filled, __width, __length):
+        Rectangle.__init__(self, __color, __filled, __width, __length)
+
+
+class EquilateralTriangle(Shape):
+    def __init__(self, __color, __filled, __side):
+        Shape.__init__(self, __color, __filled)
+        self.__side = __side
+
+    def get_sidelength(self):
+        return self.__sidelength
+
+    def set_sidelength(self, side):
+        self.__side = side
+
+
+
+
+
+
+
+
+
